c1449f15b08bc8a8bc3359fb610285f3e11664c0
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2023-11-18 19:07:51 +0100

    Added Megalodon class

---
+++ src/main/java/org/example/model/Megalodon.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class Megalodon extends Oviparous implements Swimming {
+    public Megalodon(String name, boolean gender, float weight, float height, int age) {
+        super(name, gender, weight, height, age);
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
