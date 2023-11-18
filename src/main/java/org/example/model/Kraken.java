878968117027f49362160e6bec329137b90fb510
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2023-11-18 19:06:47 +0100

    Added Kraken class

---
+++ src/main/java/org/example/model/Kraken.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class Kraken extends Oviparous implements Swimming {
+    public Kraken(String name, boolean gender, float weight, float height, int age) {
+        super(name, gender, weight, height, age);
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
