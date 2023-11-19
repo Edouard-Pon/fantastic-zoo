4cb63594df9c332ec638e7d62387b15250e8c4ca
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-11-19 05:31:49 +0100

    Added Tests for Enclosure and Aviary class

---
+++ src/test/java/org/example/model/AviaryTest.java
@@ -0,0 +1,60 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AviaryTest {
+    @Test
+    public void testAddCreatures() {
+        Lycanthrope l1 = new Lycanthrope("L1", true, 50, 25, 20);
+        Lycanthrope l2 = new Lycanthrope("L2", false, 35, 22, 15);
+        Dragon d1 = new Dragon("D1", true, 450, 350, 250);
+        Dragon d2 = new Dragon("D2", false, 450, 350, 250);
+
+        Aviary a1 = new Aviary("A1", "1000x1000", 5, 1000);
+
+        a1.addCreatures(l1, d1, d2, l2);
+
+        assertEquals(2, a1.getNumberOfCreatures(), "Number of creatures");
+        assertEquals(d1.getClass().getName(), a1.getCreaturesList().get(0).getClass().getName(), "Creature Type");
+        assertEquals(d2.getClass().getName(), a1.getCreaturesList().get(1).getClass().getName(), "Creature Type");
+
+        Lycanthrope l3 = new Lycanthrope("L3", false, 35, 22, 15);
+        Dragon d3 = new Dragon("D3", true, 450, 350, 250);
+
+        a1.addCreatures(d3, l3);
+
+        assertEquals(3, a1.getNumberOfCreatures(), "Number of creatures");
+        assertEquals(d1.getClass().getName(), a1.getCreaturesList().get(0).getClass().getName(), "Creature Type");
+        assertEquals(d2.getClass().getName(), a1.getCreaturesList().get(1).getClass().getName(), "Creature Type");
+        assertEquals(d3.getClass().getName(), a1.getCreaturesList().get(2).getClass().getName(), "Creature Type");
+    }
+
+    @Test
+    public void testMaintain() {
+        Dragon d1 = new Dragon("D1", true, 450, 350, 250);
+        Dragon d2 = new Dragon("D2", false, 450, 350, 250);
+
+        Aviary a1 = new Aviary("A1", "1000x1000", 5, 1000);
+
+        a1.addCreatures(d1, d2);
+
+        assertFalse(a1.isRoofBroken(), "Roof is not broken");
+
+        a1.setRoofBroken(true);
+
+        assertTrue(a1.isRoofBroken(), "Roof is broken");
+
+        a1.maintain();
+
+        assertTrue(a1.isRoofBroken(), "Roof is broken");
+
+        a1.removeCreatures();
+        a1.setCleanlinessLevel(false);
+        a1.maintain();
+
+        assertFalse(a1.isRoofBroken(), "Roof is not broken");
+        assertEquals(0, a1.getNumberOfCreatures(), "Aviary is empty with 0 creatures");
+    }
+}
---
+++ src/test/java/org/example/model/EnclosureTest.java
@@ -0,0 +1,50 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EnclosureTest {
+    @Test
+    public void testAddCreatures() {
+        Lycanthrope l1 = new Lycanthrope("L1", true, 50, 25, 20);
+        Lycanthrope l2 = new Lycanthrope("L2", false, 35, 22, 15);
+        Lycanthrope l3 = new Lycanthrope("L3", true, 55, 20, 25);
+
+        Enclosure e1 = new Enclosure("Lycanthropes Enclosure", "150x150", 5);
+
+        e1.addCreatures(l1, l2, l3);
+
+        assertEquals(3, e1.getNumberOfCreatures(), "Number of creatures in enclosure");
+
+        Lycanthrope l4 = new Lycanthrope("L4", true, 55, 20, 25);
+
+        e1.addCreatures(l4);
+
+        assertEquals(4, e1.getNumberOfCreatures(), "Number of creatures in enclosure");
+
+        Lycanthrope l5 = new Lycanthrope("L5", true, 55, 20, 25);
+        Lycanthrope l6 = new Lycanthrope("L6", true, 55, 20, 25);
+
+        e1.addCreatures(l5, l6);
+
+        assertEquals(5, e1.getMaxCreaturesNumber(), "Max creatures number in enclosure");
+        assertEquals(5, e1.getNumberOfCreatures(), "Number of creatures in enclosure");
+    }
+
+    @Test
+    public void testRemoveCreatures() {
+        Lycanthrope l1 = new Lycanthrope("L1", true, 50, 25, 20);
+        Lycanthrope l2 = new Lycanthrope("L2", false, 35, 22, 15);
+        Lycanthrope l3 = new Lycanthrope("L3", true, 55, 20, 25);
+
+        Enclosure e1 = new Enclosure("Lycanthropes Enclosure", "150x150", 5);
+
+        e1.addCreatures(l1, l2, l3);
+        e1.removeCreatures();
+
+        assertEquals(0, e1.getNumberOfCreatures(), "Number of creatures");
+        assertTrue(e1.getCreaturesList().isEmpty());
+    }
+}
