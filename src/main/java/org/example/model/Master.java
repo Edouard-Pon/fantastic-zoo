1e217afef7014e9d23d7b39b987088572950a41e
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-11-19 05:35:31 +0100

    Added Master class

---
+++ src/main/java/org/example/model/Master.java
@@ -0,0 +1,29 @@
+package org.example.model;
+
+public class Master {
+    private String name;
+    private boolean gender;
+    private int age;
+
+    public Master(String name, boolean gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
