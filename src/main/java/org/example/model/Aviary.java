87c4673d0a723a71a0da669c7d9e4d059313a88b
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-11-19 05:30:27 +0100

    Added Aviary class

---
+++ src/main/java/org/example/model/Aviary.java
@@ -0,0 +1,51 @@
+package org.example.model;
+
+public class Aviary extends Enclosure {
+    private int height;
+    private boolean roofBroken; // TODO Replace with Enum and rename it to roofStatus
+
+    public Aviary(String name, String area, int maxCreaturesNumber, int height) {
+        super(name, area, maxCreaturesNumber);
+        roofBroken = false;
+        this.height = height;
+    }
+
+    @Override
+    public void maintain() {
+        if (!super.cleanlinessLevel() && super.getCreaturesList().isEmpty()) {
+            super.maintain();
+            roofBroken = false;
+        }
+    }
+
+    @Override
+    public void addCreatures(Creature... creatures) {
+        if (creatures == null) return;
+        String creatureType = "";
+
+        for (Creature creature : creatures) {
+            if (creature instanceof Flying) {
+                creatureType = creature.getClass().getName();
+                break;
+            }
+        }
+
+        if (this.getNumberOfCreatures() != 0) {
+            creatureType = this.getCreaturesList().get(0).getClass().getName();
+        }
+
+        for (Creature creature : creatures) {
+            if (creature instanceof Flying && creature.getClass().getName().equals(creatureType)) {
+                super.addCreatures(creature);
+            }
+        }
+    }
+
+    public boolean isRoofBroken() {
+        return roofBroken;
+    }
+
+    public void setRoofBroken(boolean roofBroken) {
+        this.roofBroken = roofBroken;
+    }
+}
