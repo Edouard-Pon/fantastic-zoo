f7c25d2effc95d66d7f4a051dab63e85783bf8d2
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2023-12-03 17:23:26 +0100

    Added methods in Master class

--- src/main/java/org/example/model/management/Master.java
+++ src/main/java/org/example/model/management/Master.java
@@ -1,5 +1,10 @@
 package org.example.model.management;
 
+import org.example.model.creatures.Creature;
+import org.example.model.spaces.Enclosure;
+
+import java.util.HashMap;
+
 public class Master {
     private String name;
     private boolean gender;
@@ -9,6 +14,25 @@ public class Master {
         this.name = name;
         this.gender = gender;
         this.age = age;
+    }
+
+    public HashMap<String, String> showCharacteristicsOfEnclosure(Enclosure e) {
+        return  e.getCharacteristics();
+    }
+
+    public void feedCreatures(Enclosure e) {
+        e.feedCreatures();
+    }
+
+    public void maintainCreatures(Enclosure e) {
+        e.maintain();
+    }
+
+    public void transferCreatures(Enclosure e1, Enclosure e2) {
+        if(e1.getNumberOfCreatures() <= e2.getMaxCreaturesNumber()) {
+            e2.addCreatures(e1.getCreaturesList().toArray(new Creature[0]));
+            e1.removeCreatures();
+        }
     }
 
     public String getName() {
