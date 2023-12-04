4f80f197a90d6c499a3b548c5f3345671a18b478
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-12-04 01:23:48 +0100

    Added UI popup for add enclosure

--- src/main/java/org/example/view/simulation/SimulationController.java
+++ src/main/java/org/example/view/simulation/SimulationController.java
@@ -3,7 +3,9 @@ package org.example.view.simulation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.example.model.data.Data;
 import org.example.view.manager.SceneManager;
 import org.example.viewmodel.simulation.SimulationViewModel;
@@ -14,6 +16,20 @@ public class SimulationController {
     private Label lblWarning;
     @FXML
     private Label lblZooName;
+    @FXML
+    private TextField txtEnclosureName;
+    @FXML
+    private TextField txtEnclosureArea;
+    @FXML
+    private TextField txtEnclosureMaxCreatures;
+    @FXML
+    private TextField txtEnclosureHeight;
+    @FXML
+    private TextField txtEnclosureDepth;
+    @FXML
+    private TextField txtEnclosureWaterSalinity;
+    @FXML
+    private ComboBox<String> cmbEnclosureType;
 
     public SimulationController() {
         viewModel = new SimulationViewModel();
@@ -33,7 +49,8 @@ public class SimulationController {
         switch (((Button) event.getSource()).getId()) {
             // TODO add handlers for buttons
             case "btnMainMenu" -> SceneManager.getInstance().showScene("MainView");
-//            case "btnAddEnclosure" -> SceneManager.getInstance().showScene("");
+            case "btnPopupAddEnclosure" -> SceneManager.getInstance().showPopup("AddEnclosureView");
+//            case "btnAddEnclosure" -> ;
 //            case "btnRemoveEnclosure" -> SceneManager.getInstance().showScene("");
 //            case "btnMaintain" -> SceneManager.getInstance().showScene("");
 //            case "btnViewEnclosure" -> SceneManager.getInstance().showScene("");
@@ -42,6 +59,33 @@ public class SimulationController {
 
     private void bindLblZooName() {
         lblZooName.textProperty().bind(viewModel.currentZooNameProperty());
+    }
+
+    @FXML
+    public void handleEnclosureType(ActionEvent event) {
+        switch (cmbEnclosureType.getValue()) {
+            case "Aquarium" -> {
+                txtEnclosureWaterSalinity.setDisable(false);
+                txtEnclosureDepth.setDisable(false);
+                txtEnclosureHeight.setDisable(true);
+                txtEnclosureHeight.setText("");
+            }
+            case "Aviary" -> {
+                txtEnclosureHeight.setDisable(false);
+                txtEnclosureWaterSalinity.setDisable(true);
+                txtEnclosureDepth.setDisable(true);
+                txtEnclosureWaterSalinity.setText("");
+                txtEnclosureDepth.setText("");
+            }
+            case "Default" -> {
+                txtEnclosureWaterSalinity.setDisable(true);
+                txtEnclosureHeight.setDisable(true);
+                txtEnclosureDepth.setDisable(true);
+                txtEnclosureWaterSalinity.setText("");
+                txtEnclosureHeight.setText("");
+                txtEnclosureDepth.setText("");
+            }
+        }
     }
 
     public void updateData() {
