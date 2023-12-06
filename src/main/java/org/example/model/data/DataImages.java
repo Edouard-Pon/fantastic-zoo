ea7d050f2286e54a9841f3410f966f85393bc011
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-12-06 02:35:26 +0100

    Added DataImages class

---
+++ src/main/java/org/example/model/data/DataImages.java
@@ -0,0 +1,47 @@
+package org.example.model.data;
+
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataImages {
+    private static DataImages instance = null;
+    private final String IMAGE_PATH = "file:src/main/resources/org/example/assets/images/";
+    private final String IMAGE_EXTENSION = ".jpeg";
+    private final String IMAGE_UNICORN = "unicorn";
+    private final String IMAGE_DRAGON = "dragon";
+    private final String IMAGE_KRAKEN = "kraken";
+    private final String IMAGE_SIREN = "siren";
+    private final String IMAGE_NYMPH = "nymph";
+    private final String IMAGE_LYCANTHROPE = "lycanthrope";
+    private final String IMAGE_PHOENIX = "phoenix";
+    private final String IMAGE_MEGALODON = "megalodon";
+    private Map<String, Image> imagesMap;
+
+    private DataImages() {
+        imagesMap = new HashMap<>();
+    }
+
+    public static DataImages getInstance() {
+        if (instance == null) {
+            instance = new DataImages();
+        }
+        return instance;
+    }
+
+    public void loadImages() {
+        imagesMap.put(IMAGE_UNICORN, new Image(IMAGE_PATH + IMAGE_UNICORN + IMAGE_EXTENSION));
+        imagesMap.put(IMAGE_DRAGON, new Image(IMAGE_PATH + IMAGE_DRAGON + IMAGE_EXTENSION));
+        imagesMap.put(IMAGE_KRAKEN, new Image(IMAGE_PATH + IMAGE_KRAKEN + IMAGE_EXTENSION));
+        imagesMap.put(IMAGE_SIREN, new Image(IMAGE_PATH + IMAGE_SIREN + IMAGE_EXTENSION));
+        imagesMap.put(IMAGE_NYMPH, new Image(IMAGE_PATH + IMAGE_NYMPH + IMAGE_EXTENSION));
+        imagesMap.put(IMAGE_LYCANTHROPE, new Image(IMAGE_PATH + IMAGE_LYCANTHROPE + IMAGE_EXTENSION));
+        imagesMap.put(IMAGE_PHOENIX, new Image(IMAGE_PATH + IMAGE_PHOENIX + IMAGE_EXTENSION));
+        imagesMap.put(IMAGE_MEGALODON, new Image(IMAGE_PATH + IMAGE_MEGALODON + IMAGE_EXTENSION));
+    }
+
+    public Image getImage(String imageName) {
+        return imagesMap.get(imageName);
+    }
+}
